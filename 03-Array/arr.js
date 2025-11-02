9246b1de04ab15587e698edd3a9f4cad08c7f703
Ajith PM <ajithpmdwaraka@gmail.com>
2025-11-02 15:08:03 +0530

    DS Array

---
+++ 03-Array/arr.js
@@ -0,0 +1,7 @@
+const numberArray = [1,2,3,4]
+
+const stringArray = ['a', 'b', 'c', 'd']
+
+const boolArray = [true, false]
+
+const mixedArray = [1, 'a', true, null, undefined, {name: 'John'}, [1,2,3]]
